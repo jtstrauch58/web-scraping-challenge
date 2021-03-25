0b83edf6bd38b5cee0d5b654d9b7c731afd9a12b
Jeremy Strauch <jtstrauch58@gmail.com>
2021-03-25 12:08:12 -0500

    latest changes 3.25 12:08

--- app.py
+++ app.py
@@ -1,56 +1,31 @@
-# import necessary libraries
-from flask import Flask, render_template
-
-# Import our pymongo library, which lets us connect our Flask app to our Mongo database.
-import pymongo
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scraping
+import pandas as pd
 
 # create instance of Flask app
 app = Flask(__name__)
 
-# Create connection variable
-conn = 'mongodb://localhost:27017'
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
-# Pass connection to the pymongo instance.
-client = pymongo.MongoClient(conn)
 
-# Connect to a database. Will create one if not already available.
-db = client.team_db
-
-# create route that renders index.html template
+# Route to render index.html template using data from Mongo
 @app.route("/")
-def echo():
-    return render_template("index.html", text="Serving up cool text from the Flask server!!")
-
-def index():
-    team_list = ["Jumpers", "Dunkers", "Dribblers", "Passers"]
-    return render_template("index.html", list=team_list)
-
-def index():
-    player_dictionary = {"player_1": "Jessica",
-                         "player_2": "Mark"}
-    return render_template("index.html", dict=player_dictionary)
-
-def index():
-    # Store the entire team collection in a list
-    teams = list(db.team.find())
-    print(teams)
-
-    # Return the template with the teams list passed in
-    return render_template('index.html', teams=teams)
-
-
-@app.route("/")
-def index():
-    listings = mongo.db.listings.find_one()
-    return render_template("index.html", listings=listings)
-
+def home():
+    
+    data_mars = mongo.db.data_mars.find_one()
+    
+    return render_template("index.html", data=data_mars)
 
 @app.route("/scrape")
-def scraper():
-    listings = mongo.db.listings
-    listings_data = scrape_phone.scrape()
-    listings.update({}, listings_data, upsert=True)
+def scrape():
+    
+    nasa_news = scraping.scrape_info()
+    mongo.db.data_mars.update({}, nasa_news, upsert=True)
+
     return redirect("/", code=302)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
--- scraping.py
+++ scraping.py
@@ -1,18 +1,179 @@
-from bs4 import BeautifulSoup
 import requests
 import pymongo
+from splinter import Browser
+from bs4 import BeautifulSoup as bs
+from webdriver_manager.chrome import ChromeDriverManager
+from re import search
+import time
+import pandas as pd
+
+def scrape_info():
+
+# ******************************************************************************
+# ******************************************************************************
+# Fetch latest articles from NASA site using Beautifulsoup
+# ******************************************************************************
+# ****************************************************************************** 
+
+    # Set up requests for NASA data 
+    url_nasa = 'https://mars.nasa.gov/#news_and_events'
+    response_nasa = requests.get(url_nasa)
+
+    # Create BeautifulSoup object; parse with 'html.parser'
+    soup_nasa = bs(response_nasa.text, 'html.parser')
+
+    # Find top stories using find_all, h3 tag and class - 'title'
+    results_nasa = soup_nasa.find_all('h3', class_='title')
+
+    # set up list and dictionary to store the NASA articles
+    nasa_news = {}
+    
+    article_title = []
+    article_link = []
+
+    # assign article title and url to lists
+    for result in results_nasa:
+
+        try:
+            # Identify and return title of listing
+            title = result.a.text
+
+            # Identify and return link to listing
+            link = result.a['href']
+            if search('http', link):
+                link = link
+            else:
+                link = url_nasa + link
+
+            # Append results only if title and link are available
+            if (title and link):
+                article_title.append(title)
+                article_link.append(link)
+        except AttributeError as e:
+            print(e)
+
+    # import lists into nasa_news dictionary
+    nasa_news['nasa'] = {'title': article_title, 'src': article_link}
+
+# ******************************************************************************
+# ******************************************************************************
+# Fetch latest image url from JPL site using  splinter
+# ******************************************************************************
+# ******************************************************************************
+
+    # Setup splinter
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+
+    # Retrieve page with the requests module
+    url_jpl = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    browser.visit(url_jpl)
+
+    # click on "FULL IMAGE" to gain access to the featured picture data
+    browser.links.find_by_partial_text('FULL IMAGE').click()
+
+    # assign image url to variable
+    mars_img_url = browser.find_by_css('img.headerimage.fade-in')['src']
+
+    browser.quit()
+
+    mars_img = {'title': 'JPL Features Mars Image', 'src': mars_img_url}
+
+    nasa_news['mars'] = {'title': 'JPL Featured Image', 'src': mars_img_url}
+    # nasa_news['title_JPL'] = 'JPL Featured Mars Image'
+    # nasa_news['src_JPL'] = mars_img_url
 
 
-# Setup connection to mongodb
-conn = "mongodb://localhost:27017"
-client = pymongo.MongoClient(conn)
+# ******************************************************************************
+# ******************************************************************************
+# Fetch Mars hemisphere image urls from astogeology site using splinter
+# and beautifulsoup
+# ******************************************************************************
+# ******************************************************************************
 
-# Select database and collection to use
-db = client.store_inventory
-produce = db.produce
 
+    # Setup splinter
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
 
-url = 'https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-print(soup.prettify())
+    # Use splinter to capture hemisphere data
+    url_mars_hemi = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+
+    # Setup dictionary and list variables
+    img_data = {}
+    title = []
+    img = []
+
+    # parse url to add to the image cache links
+    new = url_mars_hemi.split('/')
+    url_recon = new[0] + '//' + new[2]
+
+    # start loop through four hemispheres
+    # use splinter to select each image url in order to capture the source url correctly
+    # use beautifulsoup to capture data
+
+    hemis = ['Cerberus', 'Schiaparelli', 'Syrtis', 'Valles']
+
+    for i in range(4):
+        
+        url_mars_hemi = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+        browser.visit(url_mars_hemi)
+
+        time.sleep(5)
+
+        browser.links.find_by_partial_text(hemis[i]).click()
+        new_url = browser.url
+        response_title = requests.get(new_url)
+        # Create BeautifulSoup object; parse with 'html.parser'
+
+        soup_title = bs(response_title.text, 'html.parser')
+        # results are returned as an iterable list
+
+        hemi_title = soup_title.find('head')
+        title.append(hemi_title.title.text)
+
+        hemi_img = soup_title.find('img', class_='thumb')
+        img.append(url_recon + hemi_img['src'])
+        
+        i += 1
+
+    browser.quit()
+
+    # img_data['title'].append(title)
+    # img_data['img'].append(img)
+
+    for index in range(len(title)):
+        title[index] = title[index].split(" | ")[0]
+        title[index] = title[index].replace(' Enhanced','')
+
+    nasa_news['hemi'] = {'name': title, 'src': img}
+    # nasa_news['hemi_title'] = img_data['title']
+    # nasa_news['hemi_img'] = img_data['img']
+
+# ********************************************************************************
+# ********************************************************************************
+# Get data tables about Mars from Space Facts site
+# ********************************************************************************
+# ********************************************************************************
+
+    # Use pandas to read the tables on the site
+    url_mars = 'https://space-facts.com/mars/'
+    tables = pd.read_html(url_mars)
+    mars_data_tb = tables[0]
+    mars_earth_tb = tables[1]
+
+    # Rename columns
+    mars_data_tb.rename(columns={0:'Attribute', 1:'Value'}, inplace = True)
+    mars_data_tb.set_index('Attribute', inplace = True)
+
+    mars_earth_tb.rename(columns={'Mars - Earth Comparison':'Attribute'}, inplace = True)
+    mars_earth_tb.set_index('Attribute', inplace = True)
+
+    # generate HTML code for each table
+    mars_earth_html = mars_earth_tb.to_html(header = False, index_names = False, justify = 'left')
+    mars_data_html = mars_data_tb.to_html(header = False, index_names = False, justify = 'left')
+
+    nasa_news['mars_facts'] = mars_data_html
+    nasa_news['mars_earth_facts'] = mars_earth_html
+
+    return nasa_news
